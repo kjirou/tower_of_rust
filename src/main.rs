76b2e68d65330bd3f13c488037f5f231f1d85e23
kjirou <sorenariblog@gmail.com>
2021-07-30 00:54:42 +0900

    Save) いろいろわからない
    - move_ownership で、b の参照がエラーにならない？
    - reference_check_2 で、*a_mut_ref1 = 20; になるとコンパイルエラーになるのがわかってない。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+    println!("-- main --");
+    move_ownership();
+    reference_check_2();
+}
+
+fn move_ownership() {
+    println!("-- move_ownership --");
+    let a = 1;
+    let b = a;
+    println!("{}", a);
+    println!("{}", b);
+}
+
+fn reference_check() {
+    println!("-- reference_check --");
+    let a = 10;               // immutable object
+    let aref1 = &a;           // reference
+    let aref2 = &a;           // reference
+    println!("{}, {}, {}", a, aref1, aref2); // borrow check!! - OK
+}
+
+fn reference_check_2() {
+    println!("-- reference_check_2 --");
+    let mut a = 10;           // mutable object
+    let a_ref1 = &a;          // reference
+    let a_mut_ref1 = &mut a;  // mutable reference
+    let a_mut_ref2 = &mut a;  // mutable refernece
+    *a_mut_ref2 = 20;         // assign
+    println!("{}", a);        // borrow check!! - OK
 }
