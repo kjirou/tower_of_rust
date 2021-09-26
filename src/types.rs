6638d88515f9dc764742a6c0b77b7f5415e67466
kjirou <sorenariblog@gmail.com>
2021-09-26 20:09:55 +0900

    Remove a TODO which is probably unsolvable

--- src/types.rs
+++ src/types.rs
@@ -23,5 +23,4 @@ pub type FieldElementPosition = (u32, u32);
 /// 
 /// It consists of (x, y, id_of_field_object).  
 /// The (x, y) part is equal to a `FieldElementPosition`.
-// TODO: 他の type の型を使えないか。
 pub type FieldObjectLocation = (u32, u32, String);
