b838420309600b293e8eda5d4d19f1cc632bf806
kjirou <sorenariblog@gmail.com>
2021-10-04 15:35:42 +0900

    wip

--- src/models/field_effect.rs
+++ src/models/field_effect.rs
@@ -2,11 +2,14 @@
 pub struct FieldEffect {
     /// It is unique throughout the application.
     id: String,
+    /// It is the number of frames since it was created. Count from 0.
+    number_of_frames: u64,
 }
 impl FieldEffect {
     pub fn new(id: &str) -> Self {
         Self {
             id: id.to_string(),
+            number_of_frames: 0,
         }
     }
     pub fn get_id(&self) -> String {
