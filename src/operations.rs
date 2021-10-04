02d0d362d925802c59eb07d33e80218add098099
kjirou <sorenariblog@gmail.com>
2021-10-04 13:39:13 +0900

    Place the field effect in the current direction

--- src/operations.rs
+++ src/operations.rs
@@ -21,10 +21,10 @@ pub fn moves_one_step(field: &mut Field, game: &mut Game, direction: &FourDirect
     }
 }
 
-// TODO: 向いている方向へ効果を配置する。
 pub fn makes_attack(id_generator: &mut IdGenerator, field: &mut Field, game: &Game) {
     if let Some(operation_target_location) = &game.operation_target_location {
-        if let Ok(position) = translate_position_by_direction(&field.get_rectangle_size(), &operation_target_location.0, &FourDirection::Up) {
+        let operation_target = field.find_field_object(operation_target_location).unwrap();
+        if let Ok(position) = translate_position_by_direction(&field.get_rectangle_size(), &operation_target_location.0, &operation_target.direction) {
             field.place_field_effect(&position, FieldEffect::new(&id_generator.generate_for_field_effect()));
         }
     }
