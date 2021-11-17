a87dd2cf199014acfec7bd81153d531c0a570b36
kjirou <sorenariblog@gmail.com>
2021-11-17 09:56:45 +0900

    wip: create_id_map_from_transitions のテストから

--- src/models/field_effect_combination.rs
+++ src/models/field_effect_combination.rs
@@ -4,7 +4,6 @@ use crate::enums::{FourDirection};
 use crate::id_generator::IdGenerator;
 use crate::types::{FieldElementPosition, FieldEffectLocation, XYVector};
 
-
 // TODO: 少なくとも、FieldObject の生成 -> 移動 -> 衝突で消滅 or 時間で消滅 の遷移が定義できないといけない。
 
 #[derive(Debug)]
@@ -33,7 +32,8 @@ pub enum TransitionKind {
 #[derive(Debug)]
 pub struct FieldEffectCombination {
     pub direction: FourDirection,
-    pub id_map: HashMap<u32, FieldEffectLocation>,
+    /// It means something like this: `<inner_field_object_id, (outer_field_object_id, Option<FieldElementPosition exists when this is placed>)>`.
+    pub id_map: HashMap<u32, (String, Option<FieldElementPosition>)>,
     /// It is the number of frames since it was created. Count from 0.
     pub number_of_frames: u64,
     pub starting_point: FieldElementPosition,
@@ -41,7 +41,7 @@ pub struct FieldEffectCombination {
 }
 impl FieldEffectCombination {
     // TODO: 多くの動きが設定できるようにする。
-    pub fn new(id_generator: IdGenerator, starting_point: FieldElementPosition, direction: FourDirection) -> Self {
+    pub fn new(id_generator: &mut IdGenerator, starting_point: FieldElementPosition, direction: FourDirection) -> Self {
         let transitions: Vec<TransitionKind> = vec![
             TransitionKind::Create {
                 inner_field_effect_id: 1,
@@ -60,18 +60,33 @@ impl FieldEffectCombination {
             },
         ];
 
-        // TODO: Create を拾って、外向けの FieldEffect 用の id を生成する。
-
-        let mut id_map: HashMap<u32, FieldEffectLocation> = HashMap::new();
         Self {
-            id_map: id_map,
+            id_map: Self::create_id_map_from_transitions(id_generator, &transitions),
             starting_point,
             direction,
             number_of_frames: 0,
             transitions,
         }
     }
-    fn prepare_field_effect_ids() -> Vec<String> {
-        vec![]
+    fn create_id_map_from_transitions(
+        id_generator: &mut IdGenerator, transitions: &Vec<TransitionKind>
+    ) -> HashMap<u32, (String, Option<FieldElementPosition>)> {
+        let mut inner_ids: Vec<u32> = vec![];
+        for transition in transitions.iter() {
+            match transition {
+                // TODO: 構造体を含むパターンマッチの時に、ここへ使うフィールドだけ記述したい。
+                TransitionKind::Create {inner_field_effect_id, number_of_frames: _, vector: _} => {
+                    if !inner_ids.iter().any(|e| e == inner_field_effect_id) {
+                        inner_ids.push(inner_field_effect_id.clone());
+                    }
+                },
+                _ => {},
+            }
+        }
+        let mut id_map = HashMap::new();
+        for inner_id in inner_ids.iter() {
+            id_map.insert(inner_id.clone(), (id_generator.generate_for_field_effect(), None));
+        }
+        id_map
     }
 }
